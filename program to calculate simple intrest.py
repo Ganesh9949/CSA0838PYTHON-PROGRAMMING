24d0ed72063b956dda916c54c8e784f8e57964db
Ganesh9949 <112487424+Ganesh9949@users.noreply.github.com>
2022-09-10 13:29:32 +0530

    Add files via upload

---
+++ program to calculate simple intrest.py
@@ -0,0 +1,8 @@
+def simple_intrest (p,t,r):
+    print('the principal is', p)
+    print('the time period is', t)
+    print('the rate of intrest is', r)
+    si = (p * t * r)/100
+    print('the simple intrest is' ,si)
+    return si
+simple_intrest(8,6,8)
