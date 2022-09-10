3515b2a6aa2c37e9fc2e8d2d4ecd12a0fb99d7cc
Ganesh9949 <112487424+Ganesh9949@users.noreply.github.com>
2022-09-10 13:31:13 +0530

    Add files via upload

---
+++ program to convert celsius to fahrenheit.py
@@ -0,0 +1,7 @@
+def simple_intrest (p,t,r):
+    print('the principle is' p)
+    print('the time period is' t)
+    print('the rate of intrest is' r)
+    si = (p * t * r)/100
+    print('the simple intrest is' ,si)
+    return si
